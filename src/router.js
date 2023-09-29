201dd863cf676a971a1ab513925ae6bfabf4c272
Jean Abed <jabed@atlassian.com>
2023-09-29 15:10:20 +1000

    migrate from vue cli to vite

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ Vue.use(Router);
 
 const router = new Router({
   mode: "history",
-  base: process.env.BASE_URL,
+  base: import.meta.env.BASE_URL,
   routes: [
     { path: '/', redirect: '/graph' },
 
